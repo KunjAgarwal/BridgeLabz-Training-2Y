7f4a6640f2ad601f018f2d87a7ad1635b67f87fe
KunjAgarwal <kunjagarwal051@gmail.com>
2025-08-21 02:09:23 +0530

    Update AverageCalculator.java

--- AverageCalculator.java
+++ AverageCalculator.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class AverageCalculator {
     public static void main(String[] args) {
         try(Scanner scanner = new Scanner(System.in)){
-
-        // Take three numbers as input
+            
         System.out.print("Enter first number: ");
         double num1 = scanner.nextDouble();
 
@@ -14,10 +13,8 @@ public class AverageCalculator {
         System.out.print("Enter third number: ");
         double num3 = scanner.nextDouble();
 
-        // Calculate average
         double average = (num1 + num2 + num3) / 3;
 
-        // Print the average
         System.out.println("The average is: " + average);
     }
 }
